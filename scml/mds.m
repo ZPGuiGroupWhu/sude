2bf717ea863f08ea7d54bab5e283a1b5bf7a8928
Dehua Peng <2410270794@qq.com>
2023-11-27 17:34:27 +0800

    Update mds.m

--- scml/mds.m
+++ scml/mds.m
@@ -1,9 +1,15 @@
 function embedX = mds (X, no_dims)
-% Multidimensional scaling (MDS).
+% This function performs MDS embedding.
+%
+%   Parameters are:
+%
+%   'X'       - N by D matrix. Each row in X represents an observation.
+%   'no_dims' - A positive integer specifying the number of dimension of the representation Y.
+
 [n,~] = size(X);
 D = pdist2(X,X).^2;
 sumd = mean(D);
-sumD = mean(D(:));
+sumD = mean(sumd);
 B = zeros(n,n);
 for i=1:n
     for j=i+1:n
@@ -13,4 +19,3 @@ for i=1:n
 end
 [U, value] = eigs(B,no_dims);
 embedX = U*sqrt(abs(value));
-end
