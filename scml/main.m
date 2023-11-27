de56c6f0dbc8cee478d25aa35fa0d450eed56113
Dehua Peng <2410270794@qq.com>
2023-11-27 17:40:19 +0800

    Update main.m

--- scml/main.m
+++ scml/main.m
@@ -4,13 +4,13 @@ data = csvread('benchmarks/ds3.csv');
 
 % Obtain data size and true annotations
 [~, m] = size(data);
-ref = data(:,m);
-X = data(:,1:m-1);
+ref = data(:, m);
+X = data(:, 1:m-1);
 clear data
 
 % Perform scML embedding
 t1 = clock;
-[Y,id] = scml(X,'NumNeighbors',10);
+[Y, idx, para] = scml(X,'NumNeighbors',0);
 t2 = clock;
-disp(['Elapsed time:',num2str(etime(t2,t1)),'s']);
-plotcluster2(Y,ref);
+disp(['Elapsed time:', num2str(etime(t2,t1)),'s']);
+plotcluster2(Y, ref);
